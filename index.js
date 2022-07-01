ef328ce8c779f467528c09f48127e2aaef351435
piyush-1871 <piyush1871b@gmail.com>
2022-07-01 13:35:21 +0530

    set up view engine

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ const port = 8000;
 // use express router
 app.use('/',require('./routes/index'));
 
+app.use('view engine','ejs');
+app.set('views','./views');
+
 app.listen(port,(err)=>{
     if(err){
         console.log(`Error int running the server : ${err}`);
