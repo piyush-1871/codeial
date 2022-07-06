a1e77b0c5142dc5e96a8776313648fa7e3ca06c7
piyush-1871 <piyush1871b@gmail.com>
2022-07-06 12:35:00 +0530

    add scss to the files and make it good

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,6 +1,6 @@
 module.exports.home = function(req,res){
     res.render('home',{
-        title: "home"
+        title: "Home"
     })
     // return res.end('<h1>Express is up for codeial</h1>')
 }
