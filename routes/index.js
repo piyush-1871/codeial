40b0dc9e518cbe7ba47d46ef66eed9f0b4f9c298
piyush-1871 <piyush1871b@gmail.com>
2022-08-07 13:02:11 +0530

    added forget password functionality

--- routes/index.js
+++ routes/index.js
@@ -13,7 +13,7 @@ router.use('/comments', require('./comments'));
 router.use('/likes', require('./likes'))
 
 router.use('/api', require('./api'));
-
+router.use('/reset-password',require('./reset_password'));
 // for any further routes, access from here
 // router.use('/routerName', require('./routerfile));
 
