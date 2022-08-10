233673c22222706cc9b5ca60f2794a404ad0cedc
piyush-1871 <piyush1871b@gmail.com>
2022-08-10 13:59:14 +0530

    chat engine setup done , chat r visible in console but not showing up in chat box.

--- config/chat_sockets.js
+++ config/chat_sockets.js
@@ -1,12 +1,29 @@
-module.exports.chatSockets = function(socketServer){
-    let io = require('socket.io')(socketServer);
+module.exports.chatSockets = function (socketServer) {
 
+    const io = require('socket.io')(socketServer, {
+        cors: {
+            origin: "http://localhost:8000",
+            methods: ["GET", "POST"]
+        }
+    });
+    io.on('connection', function (socket) {
+        // console.log('new connection received', socket.id);
 
-    io.sockets.on('connection', function(socket){
-        console.log('new connection recieved!', socket.id);
+        socket.on('disconnect', function () {
+            console.log('socket disconnected');
+            return;
+        });
 
-        socket.on('disconnect', function(){
-            console.log('socket disconnected!');
-        })
-    })
+        socket.on('join_room', function (data) {
+            // console.log('Joining request rec.', data);
+
+            socket.join(data.chatroom);
+
+            io.in(data.chatroom).emit('user_joined', data);
+        });
+
+        socket.on('send_message', function(data){
+            io.in(data.chatroom).emit('receive_message', data);
+        });
+    });
 }
